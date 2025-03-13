819ac6e98fe257c3d7de49a5099fca6a9cc57eb4
Dorothy Tang <dorothy_tang@epam.com>
2025-03-13 16:45:11 +0800

    modify congig file

--- cypress.config.js
+++ cypress.config.js
@@ -24,8 +24,8 @@ export default {
       });
       return config;
     },
-    baseUrl: 'https://gorest.co.in'// 配置 baseUrl
-    // auth_token: 'Bearer 2f912ac51c4ef1de590bda80c88b65eeaffbc01d3f5b4234a67c672e0722ffca', // 配置 baseUrl
+    baseUrl: 'https://gorest.co.in',// 配置 baseUrl
+    auth_token: 'Bearer 2f912ac51c4ef1de590bda80c88b65eeaffbc01d3f5b4234a67c672e0722ffca', // 配置 baseUrl
   //   env: {
   //     authToken: 'Bearer 2f912ac51c4ef1de590bda80c88b65eeaffbc01d3f5b4234a67c672e0722ffca', 
   // },
